f4d1688a6e28e2ba00464a369ec22aa467d3429c
Supremesource <115138297+Supremesource@users.noreply.github.com>
2023-05-18 12:40:35 +0200

    Update Afilepaths.hs

--- App/Data/Afilepaths.hs
+++ App/Data/Afilepaths.hs
@@ -6,19 +6,15 @@ module Orderbook.Orderbook.App.Data.Afilepaths where
   -- | Pricehistory
   -- | Log
   
-    askBookPath :: String
+
     askBookPath  :: String
                   = "/Users/janzimula/Desktop/OB/Orderbook/Orderbook/App/Doc/askbook.txt"
-    
-    bidBookPath :: String
   
     bidBookPath :: String
                   = "/Users/janzimula/Desktop/OB/Orderbook/Orderbook/App/Doc/bidbook.txt"
     
     pricePath :: String
-    pricePath :: String
                   = "/Users/janzimula/Desktop/OB/Orderbook/Orderbook/App/Doc/pricehistory.txt"
     
-    logPath :: String
     logPath  :: String
                   = "/Users/janzimula/Desktop/OB/Orderbook/Orderbook/App/Doc/Zcodeconfiguration.txt"
