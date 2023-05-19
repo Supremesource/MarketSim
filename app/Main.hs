26e8412585dde3f4fb0c766636404e5437a36add
Jan Zimula <dev@example.com>
2023-05-20 00:38:01 +0200

    stack builded

---
+++ app/Main.hs
@@ -0,0 +1,16 @@
+module Main (main) where
+
+-- import CsvLib
+
+import Lib
+
+-- Constants --
+
+-- maxOperationsWhatever = 100
+
+-- Main --
+
+main :: IO ()
+main = do
+    -- doTheWholeThing maxOperationsWhatever
+    print 2
